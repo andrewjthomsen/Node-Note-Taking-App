4895d40bd9adefadc8b472d89c86b57fdd25ca7f
andrew <andrewjthomsen@yahoo.com>
2019-07-18 12:33:02 -0700

    First initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const validator = require('validator');
+const getNotes = require('./notes');
+
+const message = getNotes();
+
+console.log(message);
+
+console.log(validator.isEmail('andrew@example.com'))
