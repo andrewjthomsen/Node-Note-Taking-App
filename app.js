3bf152621eac101c74ef96aa382ad53ad5508526
andrew <andrewjthomsen@yahoo.com>
2019-07-18 18:08:20 -0700

    Adding remove note to notes.js file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 yargs.command({
   command: "add",
@@ -18,8 +18,7 @@ yargs.command({
     }
   },
   handler: function(argv) {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
+    notes.addNote(argv.title, argv.body)
   }
 });
 
